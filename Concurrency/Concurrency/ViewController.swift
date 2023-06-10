3e1bd6c2c8b8ed9cf4acc5ec491a93cd8163b772
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2023-06-10 19:46:38 +0300

    master
    create different threads type

--- Concurrency/Concurrency/ViewController.swift
+++ Concurrency/Concurrency/ViewController.swift
@@ -12,8 +12,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
     }
-
-
 }
 
+
+func createDifferentThreadsType() {
+    
+    //third priority
+    DispatchQueue.global(qos: .utility).async {
+        Thread.sleep(forTimeInterval: 1)
+        print("Current Thread is ", Thread.current)
+        print("Doing some utility work ⛈️")
+        print("----------------------------------------------")
+    }
+    
+    //lowest priority
+    DispatchQueue.global(qos: .background).async {
+        Thread.sleep(forTimeInterval: 1)
+        print("Current Thread is ", Thread.current)
+        print("Doing some background work 🚧")
+        print("----------------------------------------------")
+    }
+    
+    //second priority
+    DispatchQueue.global(qos: .userInitiated).async {
+        Thread.sleep(forTimeInterval: 1)
+        print("Current Thread is ", Thread.current)
+        print("Doing some Initiated work 🤨")
+        print("----------------------------------------------")
+    }
+    
+    //first priority
+    DispatchQueue.global(qos: .userInteractive).async {
+        Thread.sleep(forTimeInterval: 1)
+        print("Current Thread is ", Thread.current)
+        print("Doing an Interactive task 😎")
+        print("----------------------------------------------")
+    }
+    
+    // Custom thread
+    DispatchQueue(label: "com.fayeq.concrrency", qos: .userInteractive).async {
+      Thread.sleep(forTimeInterval: 1)
+      print("Current Thread is ", Thread.current)
+      print("Doing some userInterActive work 😎")
+      print("----------------------------------------------")
+    }
+}
