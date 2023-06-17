59efe5cf887e5f1066fc6dda5bc3a1f0f6333101
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2023-06-17 19:09:24 +0300

    Add some threads tips

--- Concurreny_Async-Sync/Concurreny_Async-Sync/ViewController.swift
+++ Concurreny_Async-Sync/Concurreny_Async-Sync/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        createAsyncThread()
+//        shouldBackToMainThread()
+        workWithAsyncAfter()
     }
 
+    
+    
+    //it means that the thread is gonna work concurrently
+    func createAsyncThread() {
+        DispatchQueue.global().async {
+          for i in 0...10 {
+            Thread.sleep(forTimeInterval: 1)
+            let res = repeatElement("***", count: i)
+            print(res.joined())
+          }
+        }
+        print("Finished")
+    }
+    
+    
+    
+    func shouldBackToMainThread() {
+        DispatchQueue.global().async {
+
+          for i in 0...10 {
+
+            Thread.sleep(forTimeInterval: 0.5)
+
+            let res = repeatElement("***", count: i)
+
+            print(res.joined())
+
+          }
+
+           
+
+          DispatchQueue.main.async {
+              print("Current thread is", Thread.current)
+              print("Hello from main thread ")
+
+          }
+
+        }
+        print("Finished")
+    }
+    
+    func workWithAsyncAfter() {
+        // Global Queue (Background Threads)
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+
+            print("Hello after 2 seconds")
+
+        }
+
+        // MainQueue (Main Thread)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+
+            print("Hello after 2 seconds")
+
+        }
+
+        // Custom Queue (Background Threads) will explain later in next article
+        DispatchQueue(label: "com.fayeq.ahmed").asyncAfter(deadline: .now() + 2) {
+
+            print("Hello after 2 seconds")
+            print("Current thread is", Thread.current)
+
+        }
+
+    }
+    
 
 }
 
